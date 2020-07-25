768108a66c9277a359611cb4d2bdbeb541aac8a3
Mothusi Molorane <molorane.mothusi@gmail.com>
2020-07-25 19:31:46 +0200

    first commit

---
+++ src/main/java/com/blessy/dclmservice/DclmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blessy.dclmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DclmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DclmServiceApplication.class, args);
+	}
+
+}
