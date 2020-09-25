9f1b0680bac32df42e6056263116c8efcddbbfee
Mothusi Molorane <molorane.mothusi@gmail.com>
2020-09-25 15:47:39 +0200

    change localhost

--- src/test/java/com/blessy/dclmservice/DclmServiceApplicationTests.java
+++ src/test/java/com/blessy/dclmservice/DclmServiceApplicationTests.java
@@ -1,9 +1,18 @@
 package com.blessy.dclmservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.transaction.Transactional;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@DirtiesContext
+@ActiveProfiles("local")
 class DclmServiceApplicationTests {
 
 	@Test
